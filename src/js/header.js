5a3bad3b8c87e8e80b848c6e69e556a00223e045
Skinzin <71513905+Skinzin@users.noreply.github.com>
2022-10-13 22:15:21 -0300

    Atualização

---
+++ src/js/header.js
@@ -0,0 +1,3 @@
+document.getElementById("btnLogin").addEventListener("click", () => {
+    window.location.href = "./src/pages/login.html"
+})
---
+++ src/js/login.js
@@ -0,0 +1,5 @@
+document.getElementById("userLogin").addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    window.location.href = "./src/pages/profile.html"
+})
